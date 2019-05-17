3d07fcaa83d0913b760d1db34fbf4a9a2cba3416
LIZRAD <shotup0101@gmail.com>
2019-05-17 09:51:44 +0800

    改位置試試看

--- removeAIcops/client.lua
+++ removeAIcops/client.lua
@@ -3,7 +3,7 @@ Citizen.CreateThread(function()
         Citizen.Wait(0)
         local playerPed = GetPlayerPed(-1)
         local playerLocalisation = GetEntityCoords(playerPed)
-        ClearAreaOfCops(playerLocalisation.x, playerLocalisation.y, playerLocalisation.z, 400.0)
+        ClearAreaOfCops(playerLocalisation.x, playerLocalisation.y, 1001, 400.0)   --FiveM論壇上說要超過1000才有用 試試看
         setEveryoneIgnorePlayer(true)
-end
+    end
 end)
